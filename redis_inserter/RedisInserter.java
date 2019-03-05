a83e37ddb501456749e26f7c68f26343e9c5e4cc
tigstep <tigstep@gmail.com>
2019-03-04 20:19:34 -0800

    adding RedisInserter

---
+++ redis_inserter/RedisInserter.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import redis.clients.jedis.Jedis;
+
+public class RedisInserter {
+    public static void main(String[] args) throws java.io.IOException{
+        FileInputStream fstream = new FileInputStream("../input/input.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+        //Connecting to Redis server on localhost
+        Jedis jedis = new Jedis("127.0.0.1");
+        System.out.println("Connection to server sucessfully");
+        //set the data in redis string
+        //jedis.set("tutorial-name", "Redis tutorial");
+        // Get the stored data and print it
+        //System.out.println("Stored string in redis:: "+ jedis.get("tutorial-name"));
+        String strLine = "";
+        while ((strLine = br.readLine()) != null) {
+            String[] parts = strLine.split(",");
+            jedis.set(parts[0], parts[1]);
+        }
+        //Close the input stream
+        fstream.close();
+
+    }
+}
